aebc944acfd1dfef2cb72512f9eb947d948c021b
thomaslule <thomas@lule.fr>
2017-10-14 21:15:25 +0200

    refactor effects

--- src/effectsDictionary.js
+++ src/effectsDictionary.js
@@ -1,181 +1,60 @@
+import Tone from 'tone';
 import actions from './actionsDictionary';
 
+const toHertz = val => Tone.Frequency(val).toFrequency();
+
+const createSetNumberParamEffect = (name, text, components, initVal) => {
+  const initParams = {};
+  initParams[name] = initVal;
+  return {
+    name: `set_${name}`,
+    text: `Set ${text}`,
+    actionType: 'press',
+    components,
+    params: [
+      {
+        name,
+        text,
+        type: 'number',
+      },
+    ],
+    initParams,
+    create: (params, component) => () => {
+      component.currentParams[name] = params[name];
+      component.component[name].value = params[name];
+    },
+  };
+};
+
+const createMoveNumberParamEffect = (name, text, components, initVal) => ({
+  name: `move_${name}`,
+  text: `Move ${text}`,
+  actionType: 'move',
+  components,
+  params: [
+    {
+      name: 'sensibility',
+      text: 'Sensibility',
+      type: 'number',
+    },
+  ],
+  initParams: { sensibility: initVal },
+  create: (params, c) => (value) => {
+    c.component[name].value = toHertz(c.currentParams[name]) + (value * params.sensibility);
+  },
+});
+
 const effects = [
-  {
-    name: 'set_frequency',
-    text: 'Set frequency',
-    actionType: 'press',
-    components: ['oscillator', 'filter', 'lfo'],
-    params: [
-      {
-        name: 'frequency',
-        text: 'Frequency',
-        type: 'number',
-      },
-    ],
-    initParams: { frequency: 440 },
-    create: (params, component) => () => {
-      component.currentParams.frequency = Number(params.frequency);
-      component.component.frequency.value = Number(params.frequency);
-    },
-  },
-  {
-    name: 'move_frequency',
-    text: 'Move frequency',
-    actionType: 'move',
-    components: ['oscillator', 'filter', 'lfo'],
-    params: [
-      {
-        name: 'sensibility',
-        text: 'Sensibility',
-        type: 'number',
-      },
-    ],
-    initParams: { sensibility: 100 },
-    create: (params, component) => (value) => {
-      component.component.frequency.value = Number(component.currentParams.frequency) + (Number(value) * Number(params.sensibility));
-    },
-  },
-  {
-    name: 'set_gain',
-    text: 'Set gain',
-    actionType: 'press',
-    components: ['filter'],
-    params: [
-      {
-        name: 'gain',
-        text: 'Gain',
-        type: 'number',
-      },
-    ],
-    initParams: { gain: 0 },
-    create: (params, component) => () => {
-      component.currentParams.gain = Number(params.gain);
-      component.component.gain.value = Number(params.gain);
-    },
-  },
-  {
-    name: 'move_gain',
-    text: 'Move gain',
-    actionType: 'move',
-    components: ['filter'],
-    params: [
-      {
-        name: 'sensibility',
-        text: 'Sensibility',
-        type: 'number',
-      },
-    ],
-    initParams: { sensibility: 10 },
-    create: (params, component) => (value) => {
-      component.component.gain.value = Number(component.currentParams.gain) + (Number(value) * Number(params.sensibility));
-    },
-  },
-  {
-    name: 'set_q',
-    text: 'Set Q',
-    actionType: 'press',
-    components: ['filter'],
-    params: [
-      {
-        name: 'Q',
-        text: 'Q',
-        type: 'number',
-      },
-    ],
-    initParams: { Q: 1 },
-    create: (params, component) => () => {
-      component.currentParams.Q = Number(params.Q);
-      component.component.Q.value = Number(params.Q);
-    },
-  },
-  {
-    name: 'move_q',
-    text: 'Move Q',
-    actionType: 'move',
-    components: ['filter'],
-    params: [
-      {
-        name: 'sensibility',
-        text: 'Sensibility',
-        type: 'number',
-      },
-    ],
-    initParams: { sensibility: 10 },
-    create: (params, component) => (value) => {
-      component.component.Q.value = Number(component.currentParams.Q) + (Number(value) * Number(params.sensibility));
-    },
-  },
-  {
-    name: 'set_detune',
-    text: 'Set detune',
-    actionType: 'press',
-    components: ['oscillator'],
-    params: [
-      {
-        name: 'detune',
-        text: 'Detune',
-        type: 'number',
-      },
-    ],
-    initParams: { detune: 0 },
-    create: (params, component) => () => {
-      component.currentParams.detune = Number(params.detune);
-      component.component.detune.value = Number(params.detune);
-    },
-  },
-  {
-    name: 'move_detune',
-    text: 'Move detune',
-    actionType: 'move',
-    components: ['oscillator'],
-    params: [
-      {
-        name: 'sensibility',
-        text: 'Sensibility',
-        type: 'number',
-      },
-    ],
-    initParams: { sensibility: 100 },
-    create: (params, component) => (value) => {
-      component.component.detune.value = Number(component.currentParams.detune) + (Number(value) * Number(params.sensibility));
-    },
-  },
-  {
-    name: 'set_amplitude',
-    text: 'Set amplitude',
-    actionType: 'press',
-    components: ['lfo'],
-    params: [
-      {
-        name: 'amplitude',
-        text: 'Amplitude',
-        type: 'number',
-      },
-    ],
-    initParams: { detune: 0 },
-    create: (params, component) => () => {
-      component.currentParams.amplitude = Number(params.amplitude);
-      component.component.amplitude.value = Number(params.amplitude);
-    },
-  },
-  {
-    name: 'move_amplitude',
-    text: 'Move amplitude',
-    actionType: 'move',
-    components: ['lfo'],
-    params: [
-      {
-        name: 'sensibility',
-        text: 'Sensibility',
-        type: 'number',
-      },
-    ],
-    initParams: { sensibility: 1 },
-    create: (params, component) => (value) => {
-      component.component.amplitude.value = Number(component.currentParams.amplitude) + (Number(value) * Number(params.sensibility));
-    },
-  },
+  createSetNumberParamEffect('frequency', 'Frequency', ['oscillator', 'filter', 'lfo'], 440),
+  createMoveNumberParamEffect('frequency', 'Frequency', ['oscillator', 'filter', 'lfo'], 100),
+  createSetNumberParamEffect('gain', 'Gain', ['filter'], 0),
+  createMoveNumberParamEffect('gain', 'Gain', ['filter'], 10),
+  createSetNumberParamEffect('Q', 'Q', ['filter'], 1),
+  createMoveNumberParamEffect('Q', 'Q', ['filter'], 10),
+  createSetNumberParamEffect('detune', 'Detune', ['oscillator'], 0),
+  createMoveNumberParamEffect('detune', 'Detune', ['oscillator'], 100),
+  createSetNumberParamEffect('amplitude', 'Amplitude', ['lfo'], 0),
+  createMoveNumberParamEffect('amplitude', 'Amplitude', ['lfo'], 1),
   {
     name: 'switch_mute',
     text: 'Switch mute',
