deaa8d4f37033e2c1682ee6cabf39cb38e5c2019
thomaslule <thomas@lule.fr>
2017-10-15 14:50:58 +0200

    use kallaspriit gamepad library

--- src/controller.js
+++ src/controller.js
@@ -1,142 +1,57 @@
-const buttonsDic = [
-  { index: 0, name: 'A' },
-  { index: 1, name: 'B' },
-  { index: 2, name: 'X' },
-  { index: 3, name: 'Y' },
-  { index: 4, name: 'LB' },
-  { index: 5, name: 'RB' },
-  { index: 8, name: 'BACK' },
-  { index: 9, name: 'START' },
-  { index: 10, name: 'LSTICK' },
-  { index: 11, name: 'RSTICK' },
-  { index: 12, name: 'PAD_UP' },
-  { index: 13, name: 'PAD_DOWN' },
-  { index: 14, name: 'PAD_LEFT' },
-  { index: 15, name: 'PAD_RIGHT' },
-];
+const gamepad = new window.Gamepad();
 
-const previousButtonsState = buttonsDic.map(button => ({ index: button.index, pressed: false }));
-
-let buttonsCallbacks = buttonsDic.map(button => (
-  { index: button.index, onPress: [], onRelease: [] }
-));
-
-const axisDic = [
-  { index: 0, name: 'LSTICK_X' },
-  { index: 1, name: 'LSTICK_Y' },
-  { index: 2, name: 'RSTICK_X' },
-  { index: 3, name: 'RSTICK_Y' },
-];
-
-const previousAxisState = axisDic.map(axis => ({ index: axis.index, value: 0 }));
-
-let axisCallbacks = axisDic.map(axis => (
-  { index: axis.index, onMove: [] }
-));
-
-const triggersDic = [
-  { index: 6, name: 'LT' },
-  { index: 7, name: 'RT' },
-];
-
-const previousTriggersState = triggersDic.map(trigger => ({ index: trigger.index, value: 0 }));
-
-let triggersCallbacks = triggersDic.map(trigger => (
-  { index: trigger.index, onMove: [] }
-));
-
-const getFromIndex = (collection, index) => collection.find(entry => entry.index === index);
-const getFromName = (collection, name) => collection.find(entry => entry.name === name);
-
-const buttonPressed = (button) => {
-  getFromIndex(buttonsCallbacks, button).onPress.forEach(callback => callback());
-  getFromIndex(previousButtonsState, button).pressed = true;
+const buttons = {
+  A: 'FACE_1',
+  B: 'FACE_2',
+  X: 'FACE_3',
+  Y: 'FACE_4',
+  LB: 'LEFT_TOP_SHOULDER',
+  RB: 'RIGHT_TOP_SHOULDER',
+  BACK: 'SELECT_BACK',
+  START: 'START_FORWARD',
+  LSTICK: 'LEFT_STICK',
+  RSTICK: 'RIGHT_STICK',
+  PAD_UP: 'DPAD_UP',
+  PAD_DOWN: 'DPAD_DOWN',
+  PAD_LEFT: 'DPAD_LEFT',
+  PAD_RIGHT: 'DPAD_RIGHT',
 };
 
-const buttonReleased = (button) => {
-  getFromIndex(buttonsCallbacks, button).onRelease.forEach(callback => callback());
-  getFromIndex(previousButtonsState, button).pressed = false;
+const axis = {
+  LSTICK_X: 'LEFT_STICK_X',
+  LSTICK_Y: 'LEFT_STICK_Y',
+  RSTICK_X: 'RIGHT_STICK_X',
+  RSTICK_Y: 'RIGHT_STICK_Y',
+  LT: 'LEFT_BOTTOM_SHOULDER',
+  RT: 'RIGHT_BOTTOM_SHOULDER',
 };
 
-const updateButtonStatus = (button, pressed) => {
-  if (getFromIndex(previousButtonsState, button).pressed !== pressed) {
-    if (pressed) {
-      buttonPressed(button);
-    } else {
-      buttonReleased(button);
-    }
-  }
-};
-
-const axisMoved = (axis, value) => {
-  getFromIndex(axisCallbacks, axis).onMove.forEach(callback => callback(value));
-  getFromIndex(previousAxisState, axis).value = value;
-};
-
-const updateAxisStatus = (axis, value) => {
-  if (getFromIndex(previousAxisState, axis).value !== value) {
-    axisMoved(axis, value);
-  }
-};
-
-const triggerMoved = (trigger, value) => {
-  getFromIndex(triggersCallbacks, trigger).onMove.forEach(callback => callback(value));
-  getFromIndex(previousTriggersState, trigger).value = value;
-};
-
-const updateTriggerStatus = (trigger, value) => {
-  if (getFromIndex(previousTriggersState, trigger).value !== value) {
-    triggerMoved(trigger, value);
-  }
-};
-
-const updateStatus = (gamepad) => {
-  [0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15]
-    .forEach(index => updateButtonStatus(index, gamepad.buttons[index].pressed));
-  [0, 1, 2, 3]
-    .forEach(index => updateAxisStatus(index, gamepad.axes[index].toFixed(2)));
-  [6, 7]
-    .forEach(index => updateTriggerStatus(index, gamepad.buttons[index].value.toFixed(2)));
-};
-
-const updateLoop = () => {
-  const gamepad = navigator.getGamepads()[0];
-  if (gamepad) {
-    updateStatus(gamepad);
-  }
-  requestAnimationFrame(updateLoop);
-};
+if (!gamepad.init()) {
+  console.error('unsupported browser');
+}
 
 export const onPress = (buttonName, callback) => {
-  const buttonIndex = getFromName(buttonsDic, buttonName).index;
-  getFromIndex(buttonsCallbacks, buttonIndex).onPress.push(callback);
+  gamepad.bind(Gamepad.Event.BUTTON_DOWN, (e) => {
+    if (e.control === buttons[buttonName]) callback();
+  });
 };
 
 export const onRelease = (buttonName, callback) => {
-  const buttonIndex = getFromName(buttonsDic, buttonName).index;
-  getFromIndex(buttonsCallbacks, buttonIndex).onRelease.push(callback);
+  gamepad.bind(Gamepad.Event.BUTTON_UP, (e) => {
+    if (e.control === buttons[buttonName]) callback();
+  });
 };
 
 export const onMove = (controlName, callback) => {
-  if (axisDic.map(a => a.name).includes(controlName)) {
-    const axisIndex = getFromName(axisDic, controlName).index;
-    getFromIndex(axisCallbacks, axisIndex).onMove.push(callback);
-  } else if (triggersDic.map(a => a.name).includes(controlName)) {
-    const triggerIndex = getFromName(triggersDic, controlName).index;
-    getFromIndex(triggersCallbacks, triggerIndex).onMove.push(callback);
-  }
+  gamepad.bind(Gamepad.Event.AXIS_CHANGED, (e) => {
+    if (e.axis === axis[controlName]) callback(e.value);
+  });
 };
 
 export const resetBindings = () => {
-  buttonsCallbacks = buttonsCallbacks.map(binding => ({ ...binding, onPress: [], onRelease: [] }));
-  axisCallbacks = axisCallbacks.map(binding => ({ ...binding, onMove: [] }));
-  triggersCallbacks = triggersCallbacks.map(binding => ({ ...binding, onMove: [] }));
+  gamepad.unbind();
 };
 
-export const buttonsList = () => buttonsDic.map(dic => dic.name);
+export const buttonsList = () => Object.keys(buttons);
 
-export const axisList = () => axisDic.map(dic => dic.name);
-
-export const triggersList = () => triggersDic.map(dic => dic.name);
-
-updateLoop();
+export const axisList = () => Object.keys(axis);
