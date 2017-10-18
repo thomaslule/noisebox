c63dccddbcda92c56d77906c1cd088403b0ab189
thomaslule <thomas@lule.fr>
2017-10-18 18:12:09 +0200

    Delete action button on same line

--- src/Bindings/Binding/Action.jsx
+++ src/Bindings/Binding/Action.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
-import { Label, Button } from '../../Shared';
+import { Form, FormGroup, Col, ControlLabel, FormControl, Button } from 'react-bootstrap';
 
 export default ({ action, canDelete, onDelete }) => (
   <Form horizontal onSubmit={e => e.preventDefault()}>
-    <Label label="Action" value={action} />
-    {canDelete ? <Button text="Delete" onClick={() => onDelete()} /> : null}
+    <FormGroup>
+      <Col componentClass={ControlLabel} md={3}>Action</Col>
+      <Col componentClass={FormControl.Static} md={6}>{action}</Col>
+      {canDelete ? <Col><Button onClick={() => onDelete()}>Delete</Button></Col> : null}
+    </FormGroup>
   </Form>
 );
