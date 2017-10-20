a7b18aaad14d33e16d1ccd6975b50d5a9204dc9d
thomaslule <thomas@lule.fr>
2017-10-20 18:35:52 +0200

    fix bug change component param

--- src/Components/componentsReducer.js
+++ src/Components/componentsReducer.js
@@ -8,8 +8,8 @@ export default (state = [], action) => {
   if (action.type === 'COMPONENT_DELETE') {
     return state.filter(c => c.id !== action.componentId);
   }
-  if (['COMPONENT_CHANGE_PARAM', 'COMPONENT_CHANGE_CONNECT_TO'].includes(action.type)) {
-    return state.map(c => (c.id === action.component ? componentReducer(c, action) : c));
+  if (['COMPONENT_CHANGE_PARAM'].includes(action.type)) {
+    return state.map(c => (c.id === action.componentId ? componentReducer(c, action) : c));
   }
   if (action.type === 'STATE_JSON_CHANGED') {
     state.map(c => c.type).forEach((type) => {
